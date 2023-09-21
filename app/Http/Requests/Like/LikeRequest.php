8ba3972b8dd390746416ac147057e94ecd083b3e
Morishita <gabrielmorishita@hotmail.com>
2023-09-21 17:08:53 -0300

    📝 docs: introducing insomnia collection

--- app/Http/Requests/Like/LikeRequest.php
+++ app/Http/Requests/Like/LikeRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Like;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateLikeRequest extends FormRequest
+class StoreLikeRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
